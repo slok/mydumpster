6b5ae2007550ad2b2cdb50c9d766dc378b69a71a
Xabier Larrakoetxea <slok69@gmail.com>
2014-08-09 18:00:49 +0200

    Added configuration

--- main.go
+++ main.go
@@ -2,71 +2,25 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/slok/mydumpster/mydumpster"
 	"os"
 )
 
-const (
-	dbUser = "root"
-	dbPass = ""
-	dbHost = "172.17.0.2"
-	dbPort = 3306
-	dbName = "ticketbis_dev"
-)
+const confFile = "conf.json.example"
 
 //TESTING MAIN!!!!!
 func main() {
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", dbUser, dbPass, dbHost, dbPort, dbName)
-
+	conf := mydumpster.LoadConfiguration(confFile)
+	dsn := conf.ConnectionStr()
 	db, _ := sql.Open("mysql", dsn)
 	defer db.Close()
 
+	tables := conf.GetTables(db)
+
 	// Check connection
 	mydumpster.CheckKill(db.Ping())
-
-	tableName := "modulo_pago_gateway"
-	//filters := []string{"id < 15"}
-	censorships := map[string]mydumpster.Censorship{
-		"imagen": mydumpster.Censorship{
-			Key:    "imagen",
-			Suffix: "_after",
-			//Prefix:       "beafore_",
-			//Blank:        true,
-			//Null:         true,
-			//DefaultValue: "test",
-		},
-	}
-	setTable := mydumpster.Table{
-		Db:        db,
-		TableName: "modulo_pago_gateway_set",
-	}
-
-	gatewayTable := mydumpster.Table{
-		Db:        db,
-		TableName: tableName,
-		//Filters:     filters,
-		Censorships: censorships,
-		Triggers: []mydumpster.Trigger{
-			mydumpster.Trigger{
-				TableDst:      setTable,
-				TableSrcName:  "modulo_pago_gateway",
-				TableSrcField: "gateway_set_id",
-				TableDstField: "id",
-				//DumpAll:       true,
-			},
-			mydumpster.Trigger{
-				TableDst:      setTable,
-				TableSrcName:  "modulo_pago_gateway",
-				TableSrcField: "parent_set_id",
-				TableDstField: "id",
-			},
-		},
-	}
-
-	tables := []mydumpster.Table{setTable, gatewayTable}
 
 	//mydumpster.CheckKill(mydumpster.LockTablesRead(db, "pais"))
 	//mydumpster.CheckKill(mydumpster.LockTablesWrite(db, "pais"))
@@ -77,9 +31,15 @@ func main() {
 	mydumpster.CheckKill(err)
 	defer f.Close()
 
-	f.WriteString(mydumpster.DumpHeaderStr(tables))
-	gatewayTable.WriteRows(f)
-	f.WriteString(mydumpster.DumpFooterStr(tables))
+	tableList := make([]mydumpster.Table, 0)
+	for _, v := range tables {
+		tableList = append(tableList, v)
+	}
+
+	f.WriteString(mydumpster.DumpHeaderStr(tableList))
+	gw := tables["modulo_pago_gateway"]
+	gw.WriteRows(f)
+	f.WriteString(mydumpster.DumpFooterStr(tableList))
 
 	f.Sync()
 
---
+++ mydumpster/configuration.go
@@ -0,0 +1,177 @@
+package mydumpster
+
+import (
+	"database/sql"
+	"encoding/json"
+	//"errors"
+	"fmt"
+	"os"
+)
+
+type ConfTrigger struct {
+	TableDstName string `json:"dst_table_name"`
+	FieldSrcName string `json:"src_field_name"`
+	FieldDstName string `json:"dst_field_name"`
+}
+
+type ConfCensorship struct {
+	Prefix  string `json:"prefix"`
+	Suffix  string `json:"suffix"`
+	Blank   bool   `json:"blank"`
+	Null    bool   `json:"null"`
+	Default string `json:"default"`
+}
+
+type ConfTable struct {
+	Filters    []string                   `json:"filters"`
+	Censorship map[string]*ConfCensorship `json:"censorship"`
+	Triggers   []*ConfTrigger             `json:"triggers"`
+}
+
+type ConfDatabase struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Db       string `json:"db"`
+}
+
+type Configuration struct {
+	Tables   map[string]*ConfTable `json:"tables"`
+	Database *ConfDatabase         `json:"database"`
+}
+
+func LoadConfiguration(filePath string) *Configuration {
+
+	file, err := os.Open(filePath)
+	CheckKill(err)
+
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+
+	err = decoder.Decode(&configuration)
+	CheckKill(err)
+
+	return &configuration
+}
+
+func (c *Configuration) ConnectionStr() string {
+
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8",
+		c.Database.User, c.Database.Password,
+		c.Database.Host, c.Database.Port,
+		c.Database.Db)
+}
+
+func (c *Configuration) GetTables(db *sql.DB) map[string]Table {
+
+	// Create the containers
+	var tables = make(map[string]Table)
+
+	// Init our map (We need al the table structs created so
+	// we can refer from a table to another while we parse)
+	for k, v := range c.Tables {
+		tables[k] = Table{
+			Db:          db,
+			TableName:   k,
+			Filters:     make([]string, len(v.Filters)),
+			Censorships: make(map[string]Censorship),
+			Triggers:    make([]Trigger, len(v.Triggers)),
+		}
+	}
+
+	for k, v := range c.Tables {
+		t := tables[k]
+
+		// Create the filters
+		for k, f := range v.Filters {
+			t.Filters[k] = f
+		}
+
+		// Create the censorships
+		if v.Censorship != nil {
+			for ck, cv := range v.Censorship {
+				t.Censorships[ck] = Censorship{
+					Key:          ck,
+					Suffix:       cv.Suffix,
+					Prefix:       cv.Prefix,
+					Blank:        cv.Blank,
+					Null:         cv.Null,
+					DefaultValue: cv.Default,
+				}
+			}
+		}
+
+		// Create the censorships
+		if v.Triggers != nil {
+			for tk, tv := range v.Triggers {
+				aux, ok := tables[tv.TableDstName]
+
+				// Table not declared (We create)
+				if !ok {
+					//CheckKill(errors.New("Not table in map"))
+					tables[tv.TableDstName] = Table{
+						Db:          db,
+						TableName:   tv.TableDstName,
+						Filters:     make([]string, 0),
+						Censorships: make(map[string]Censorship),
+						Triggers:    make([]Trigger, 0),
+					}
+					aux = tables[tv.TableDstName]
+				}
+
+				t.Triggers[tk] = Trigger{
+					TableDst:      aux,
+					TableSrcName:  k,
+					TableSrcField: tv.FieldSrcName,
+					TableDstField: tv.FieldDstName,
+				}
+			}
+		}
+
+	}
+	return tables
+}
+
+func (c *Configuration) PrintConfiguration() {
+	v0 := c.Database
+	fmt.Println("Database ")
+	fmt.Println("-----------")
+	fmt.Println(fmt.Sprintf("  -Host: %s", v0.Host))
+	fmt.Println(fmt.Sprintf("  -Port: %d", v0.Port))
+	fmt.Println(fmt.Sprintf("  -Passwords: %s", v0.Password))
+	fmt.Println(fmt.Sprintf("  -User: %s", v0.User))
+	fmt.Println(fmt.Sprintf("  -Db: %s", v0.Db))
+
+	fmt.Println("")
+
+	for k, v := range c.Tables {
+		fmt.Println("Table " + k)
+		fmt.Println("-----------")
+		fmt.Println("Filters:")
+		for _, f := range v.Filters {
+			fmt.Println("  -" + f)
+		}
+
+		fmt.Println("Censore:")
+		for k2, v2 := range v.Censorship {
+			fmt.Println("  -" + k2)
+			fmt.Println(fmt.Sprintf("    -Prefix: %s", v2.Prefix))
+			fmt.Println(fmt.Sprintf("    -Suffix: %s", v2.Suffix))
+			fmt.Println(fmt.Sprintf("    -Blank: %t", v2.Blank))
+			fmt.Println(fmt.Sprintf("    -Null: %t", v2.Null))
+			fmt.Println(fmt.Sprintf("    -Default: %s", v2.Default))
+
+		}
+
+		fmt.Println("Triggers:")
+		for _, v3 := range v.Triggers {
+			fmt.Println(fmt.Sprintf("  -Src field name: %s", v3.FieldSrcName))
+			fmt.Println(fmt.Sprintf("  -Dst field name: %s", v3.FieldDstName))
+			fmt.Println(fmt.Sprintf("  -Dst Table name: %s", v3.TableDstName))
+			fmt.Println("")
+		}
+
+		fmt.Println("=============================")
+	}
+}
